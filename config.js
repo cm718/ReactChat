92978d6f1127f3a9a161ca06f8a2b1e87fd34037
cm718 <cm718@icloud.com>
2019-07-30 14:53:04 -0500

    setup the config file with token and instance

---
+++ config.js
@@ -0,0 +1,7 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/ab9a9583-5dc0-44a8-8919-9630ba72daf8/token";
+
+/** swap out */
+const instanceLocator = "v1:us1:ab9a9583-5dc0-44a8-8919-9630ba72daf8";
+
+export { tokenUrl, instanceLocator }
